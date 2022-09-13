221e259b35fb0cd20ebe4725a13f4aef60db6085
OSTAR1 <110995105+OSTAR1@users.noreply.github.com>
2022-09-13 17:51:55 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,12 @@
-/*
- * File: _putchar
- * Auth: Aniseti Sanya
- *
- */
-
-#include "main.h"
-
-/**
- * main - entry point
- *
- * Return: Always 0.
- */
-int main(void)
+Int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+         Char *main = "_putchar"
+
+         While (*main)
+                    {
+                           _putchar(*main);
+                            main++;
+                     }
+         _putchar('\n');
+         return (0);
 }
