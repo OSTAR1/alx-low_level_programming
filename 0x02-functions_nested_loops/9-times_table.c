7397fb9514393c973cd9129fd0aacbe8f0257531
OSTAR1 <okpochiemeriechi@gmail.com>
2022-09-13 15:07:17 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *  * times_table - print 9 times table
+ *   *
+ *    */
+void times_table(void)
+{
+		int i;
+			int j;
+
+				for (i = 1; i <= 9; i++)
+						{
+									_putchar('0');
+											for (j = 1; j <= 9; j++)
+														{
+																		int result = i * j;
+
+																					_putchar(',');
+																								_putchar(' ');
+																											_putchar('0' + (result / 10));
+																														_putchar('0' + (result % 10));
+																																}
+													_putchar('\n');
+														}
+					_putchar('\n');
+}
+
