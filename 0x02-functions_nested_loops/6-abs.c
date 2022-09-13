8c006475f94dcde3d2830c885c163b395c0cd228
OSTAR1 <okpochiemeriechi@gmail.com>
2022-09-13 14:57:09 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * _abs - compute the absolte value
+ *   *
+ *    * @n: integer to be checked
+ *     * Return: Always 0
+ *      */
+int _abs(int n)
+{
+		if (n < 0)
+				{
+							n = (-1) * n;
+								}
+			return (n);
+}
+
