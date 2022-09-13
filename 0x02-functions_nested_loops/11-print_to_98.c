a12235b4b3ecd5fd32f064d23bdfca86041b09c1
OSTAR1 <okpochiemeriechi@gmail.com>
2022-09-13 15:25:22 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * print_to_98 - print all natural numbers
+ *   *
+ *    * @n: parameter
+ *     * Return: Always 0
+ *      */
+void print_to_98(int n)
+{
+		for (n = -111; n <= 98; n++)
+				{
+							_putchar('0' + n / 10);
+									_putchar('0' + n % 10);
+											_putchar(',');
+													_putchar(' ');
+														}
+			_putchar('\n');
+}
+
