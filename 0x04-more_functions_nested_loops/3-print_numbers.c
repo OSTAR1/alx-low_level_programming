97d740cb260125fdcaf8549ec22d016f5027693c
OSTAR1 <110995105+OSTAR1@users.noreply.github.com>
2022-09-15 20:27:33 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,31 +1,17 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * print_numbers - check thr code for betty School students.
+ * print_numbers - check the code for Betty School students.
  *
- * Return: zero
+ * Return: zero.
  */
 void print_numbers(void)
 {
-	ont a;
+	int a;
 
-	for (a = o; a <= 9; a++)
+	for (a = 0; a <= 9; a++)
 	{
 		_putchar('0' + a);
 	}
-	_putchar('\n);
-=======
- * print_numbers - a function that prints the numbers, from 0 to 9,
- * followed by a new line.
- * Return: Always 0
- */
-void print_numbers(void)
-{
-	int i = '0';
-
-	for (; i <= '9'; i++)
-		_putchar(i);
 	_putchar('\n');
->>>>>>> 4367dfba55f07144b7792186af4a28801ec8e345
 }
