2cc4855766c78ca42cf8834f6f50206e92ceae72
OSTAR1 <110995105+OSTAR1@users.noreply.github.com>
2022-09-15 20:53:21 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,32 +1,32 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- *  * more_numbers - Print numbers 0 to 14 10 times
- *   *
- *    */
+ * more_numbers - Print numbers 0 to 14 10 times
+ *
+ */
 void more_numbers(void)
 {
-		int i;
-			int j;
+	int i;
+	int j;
 
-				i = 0;
-					while (i <= 10)
-							{
-										j = 0;
-												while (j <= 14)
-															{
-																			if (j <= 9)
-																							{
-																												_putchar('0' + j);
-																															}
-																						else
-																										{
-																															_putchar('0' + j / 10);
-																																			_putchar('0' + j % 10);
-																																						}
-																									j++;
-																											}
-														_putchar('\n');
-																i++;
-																	}
+	i = 0;
+	while (i <= 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			if (j <= 9)
+			{
+				_putchar('0' + j);
+			}
+			else
+			{
+				_putchar('0' + j / 10);
+				_putchar('0' + j % 10);
+			}
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
