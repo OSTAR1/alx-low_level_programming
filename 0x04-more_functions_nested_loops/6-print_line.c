6df9935a00214901a9308dae286aa05e53219a97
OSTAR1 <okpochiemeriechi@gmail.com>
2022-09-15 12:56:56 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  * print_line - prints a line
+ *   *
+ *    * @n: number of times to print _.
+ *     */
+
+void print_line(int n);
+{
+		if (n > 0)
+				{
+							_putchar('_');
+									n--;
+										}
+			else
+					{
+								_putchar('\n');
+									}
+				_putchar('\n');
+}
