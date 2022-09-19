50851dbee632929dcb66ae0e4f82bf0276a95740
OSTAR1 <okpochiemeriechi@gmail.com>
2022-09-19 07:45:28 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverse a string
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		_putchar(*s);
+		_putchar('\n');
+		_putchar(s[i]);
+	}
+}
