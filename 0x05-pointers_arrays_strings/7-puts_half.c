973ebe1d96c6e00da27cbe2531b7b7d18d381d79
OSTAR1 <okpochiemeriechi@gmail.com>
2022-09-19 10:48:24 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - print second half of a string
+ *
+ * @str: string
+ * @n: parameter
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i, j, n, m;
+
+	if (strlen(str) % 2 == 0)
+	{
+		for (i = strlen(str) / 2; i < string_length(str); i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (strlen(str) % 2 != 0)
+	{
+		n = (string_length(str); j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
+}
