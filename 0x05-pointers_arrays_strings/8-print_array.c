53bf82cdaab10f7c8edf8ed7e98ad2f98fdd0471
OSTAR1 <110995105+OSTAR1@users.noreply.github.com>
2022-09-19 20:00:47 +0100

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,10 +4,14 @@
  * print_array - print integers
  * @a: pass by ref
  * @n: pass by value
+ * Return: nothing
  */
 void print_array(int *a, int n)
 {
-	for (int in = 0 ; i < n ; i++)
+	int i = 0;
+
+	n = n - 1;
+	for (; i < n ; i++)
 		printf("%d, ", a[i]);
-	printf("\n");
+	printf("%d\n", a[n]);
 }
