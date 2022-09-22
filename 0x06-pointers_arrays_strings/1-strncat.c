9ba627f40fd5d8d5aa1cc5d9a40b71336cbc93a1
OSTAR1 <110995105+OSTAR1@users.noreply.github.com>
2022-09-22 07:43:31 +0100

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,10 +10,10 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != '\n')
+	while (dest[i] != '\0')
 		++i;
 	for (; j < n && src[j] ; j++)
 		dest[i + j] = src[j];
-	dest[i + j] = '\n';
+	dest[i + j] = '\0';
 	return (dest);
 }
