560fdcf5b9bbb4b157c9cb1ca1e8e659ef48720e
OSTAR1 <110995105+OSTAR1@users.noreply.github.com>
2022-09-22 07:30:18 +0100

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,28 @@
-nclude "main.h"
+#include "main.h"
 #include <string.h>
 
 /**
- *  * *_strcat - concatenate a string.
- *   *
- *    * @dest: string one.
- *     * @src: string two.
- *      * Return: dest.
- *       */
+ * *_strcat - concatenate a string.
+ *
+ * @dest: string one.
+ * @src: string two.
+ * Return: dest.
+ */
 char *_strcat(char *dest, char *src)
 {
-		/*char *ptr;*/
-		/*ptr = _strcat;*/
+	/*char *ptr;*/
+	/*ptr = _strcat;*/
 
-		while (*dest != '\0')
-				{
-							dest++;
-								}
-			while (*src != '\0')
-					{
-								*dest = *src;
-										dest++;
-												src++;
-													}
-				*dest = '\0';
-					return (dest);
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (dest);
 }
-
