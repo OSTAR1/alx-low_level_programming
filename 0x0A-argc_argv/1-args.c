4fc3a185ba2a50405f6fe9f9f12a69b8116b526d
OSTAR1 <okpochiemeriechi@gmail.com>
2022-09-30 11:13:17 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+/**
+ *
+ *  * main - print number of arguments passed.
+ *
+ *   *
+ *
+ *    * @argc: number of arguments.
+ *
+ *     * @argv: arguments.
+ *
+ *      *
+ *
+ *       * Return: zero.
+ *
+ *        */
+
+int main(int argc, char *argv[])
+
+{
+
+		(void)argv;
+
+
+
+			printf("%d\n", argc - 1);
+
+				return (0);
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
+
+Footer navigation
+
+Terms
