61c6e5e531c739a943f11e59fa3e7c32597b051f
OSTAR1 <110995105+OSTAR1@users.noreply.github.com>
2022-10-03 21:08:15 +0100

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,12 +12,12 @@ char *_strdup(char *str)
 {
 	char *new_str, *start;
 	int i = 0, len = 0;
-	
+
 	if (str == NULL)
-		return (NULL)
-			
+		return (NULL);
+
 	start = str;
-	
+
 	while (*str)
 	{
 		len++;
