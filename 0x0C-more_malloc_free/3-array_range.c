ce02f46b1977407462d198e63fcb3ad5e63dfadc
OSTAR1 <110995105+OSTAR1@users.noreply.github.com>
2022-10-06 20:01:48 +0100

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,17 +11,17 @@ int *array_range(int min, int max)
 {
 	int *new_array;
 	int diff, i;
-	
+
 	if (min > max)
 		return (NULL);
-	
+
 	diff = max - min;
 	new_array = malloc((diff + 1) * sizeof(int));
 	if (new_array == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i <= diff; i++)
 		new_array[i] = min++;
-	
+
 	return (new_array);
 }
