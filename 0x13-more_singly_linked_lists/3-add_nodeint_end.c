348096546484eb85882a0ce15b6f537783d0d1d4
OKPO-CHI CHIEMERIE <110995105+OSTAR1@users.noreply.github.com>
2022-10-25 20:36:13 +0100

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,58 +1,57 @@
-nclude "lists.h"
+#include "lists.h"
 
 /**
- *  * add_nodeint_end - add a new node at the end
- *   * of a linked list
- *    * @head: head of a list.
- *     * @n: n element.
- *      *
- *       * Return: address of the new element. NUll if it failed.
- *        */
+ * add_nodeint_end - add a new node at the end
+ * of a linked list
+ * @head: head of a list.
+ * @n: n element.
+ *
+ * Return: address of the new element. NUll if it failed.
+ */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 
-		listint_t *new;
+	listint_t *new;
 
-			listint_t *temp;
+	listint_t *temp;
 
-				(void)temp;
+	(void)temp;
 
-					new = malloc(sizeof(listint_t));
+	new = malloc(sizeof(listint_t));
 
-						if (new == NULL)
+	if (new == NULL)
 
-									return (NULL);
+		return (NULL);
 
-							new->n = n;
+	new->n = n;
 
-								new->next = NULL;
+	new->next = NULL;
 
-									temp = *head;
+	temp = *head;
 
-										if (*head == NULL)
+	if (*head == NULL)
 
-												{
+	{
 
-															*head = new;
+		*head = new;
 
-																}
+	}
 
-											else
+	else
 
-													{
+	{
 
-																while (temp->next != NULL)
+		while (temp->next != NULL)
 
-																			{
+		{
 
-																							temp = temp->next;
+			temp = temp->next;
 
-																									}
+		}
 
-																		temp->next = new;
+		temp->next = new;
 
-																			}
-												return (*head);
+	}
+	return (*head);
 }
-
